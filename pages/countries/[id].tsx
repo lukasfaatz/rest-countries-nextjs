f14a553542ef8cb70b64dc38568c43091bd90ec8
lukas <lukas.faatz@codecamp-n.com>
2021-10-11 23:11:38 +0200

    fixed missing div

--- pages/countries/[id].tsx
+++ pages/countries/[id].tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 import axios from "axios";
 import Header from "../../components/Header";
 import { Country } from "../../models/Country";
 import { ArrowLeftIcon } from "@heroicons/react/outline";
+import Borders from "../../components/Borders";
 import CountryInformation from "../../components/CountryInformation";
 import CountryFlag from "../../components/CountryFlag";
 
@@ -12,17 +15,23 @@ interface CountryPageProps {
 }
 
 function CountryPage({ country, countries }: CountryPageProps) {
+
+
 	return (
 		<div className='dark:bg-gray-800 dark:text-white min-h-screen'>
 			<Header />
 			<div className='px-12 md:px-24'>
-				<div className='border-2 p-2 px-4  w-36 flex items-center shadow-md rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600'>
-					<ArrowLeftIcon className='h-8 w-8' />
-					<a className='text-xl mx-2'>Back</a>
+				<div className='py-16'>
+					<Link href='/'>
+						<div className='border-2 p-2 px-4  w-36 flex items-center shadow-md rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600'>
+							<ArrowLeftIcon className='h-8 w-8' />
+							<a className='text-xl mx-2'>Back</a>
+						</div>
+					</Link>
 				</div>
 				<div className='flex flex-col  md:flex-row gap-5'>
-					<CountryFlag country={country} />
-					<CountryInformation country={country} countries={countries} />
+					<CountryFlag country={country}/>
+					<CountryInformation country={country} countries={countries}/>
 				</div>
 			</div>
 		</div>
