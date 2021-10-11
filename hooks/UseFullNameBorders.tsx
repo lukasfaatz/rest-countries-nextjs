80b494402033da01333a52cb8ff6e1b393725f63
lukas <lukas.faatz@codecamp-n.com>
2021-10-11 22:51:51 +0200

    rm test hook

--- hooks/UseFullNameBorders.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react"
-export default  function useFullNameBorders() {
-    const [countryBorder, setCountryBorder] = useState([])
-
-    return [countryBorder, setCountryBorder];
-}
